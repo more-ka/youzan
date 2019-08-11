7c5d428644d55936fd0377bd16510704738779ee
more-ka <1640627628@qq.com>
2019-08-11 23:47:47 +0800

    使用mint-ui,实现商品滚动加载

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,21 +3,38 @@ import './index.css'
 import Vue from  'vue'
 import axios from 'axios'
 import url from 'js/api.js'
+import { InfiniteScroll } from 'mint-ui';
 
+Vue.use(InfiniteScroll);
 let app = new Vue({
   el: '#app',
   data:{
-    lists: null
+    lists: null,
+    pageNum: 1,
+    pageSize: 6,
+    isLoading: false,
+    allLoading: false
   },
   methods:{
     getData(){
+      if(this.allLoading) return
+      this.isLoading = true
       axios.get(url.homepage)
         .then(response=>{
-          this.lists = response.data.lists
+          if(response.data.lists.length<this.pageSize){
+            this.allLoading = true
+          }
+          if (this.lists){
+            this.lists = this.lists.concat(response.data.lists)
+          }else {
+            this.lists = response.data.lists
+          }
         })
         .catch(error=>{
           console.log(error);
         })
+      this.pageNum += 1
+      this.isLoading = false
     }
   },
   beforeMount(){

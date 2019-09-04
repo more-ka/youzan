ad13d306100ea0ba5c5b620b39193fad190f7695
more-ka <1640627628@qq.com>
2019-09-04 20:31:24 +0800

    改网址

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -21,7 +21,7 @@ let url = {
 }
 // 开发环境和真实环境的切换
 // let host = ''
-let host = 'http://rap2api.taobao.org/app/mock/7058'
+let host = 'https://www.easy-mock.com/mock/5c9c3045d172204b3a07ecb0/youzan'
 for (let key in url) {
   if(url.hasOwnProperty(key)){
     url[key] = host + url[key]
